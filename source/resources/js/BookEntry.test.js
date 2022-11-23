30ea967d53798058f404f54ae55cd51fb0683f90
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-23 15:07:57 -0800

    Fix minor issue with test expected output
    github actions VM's work from GMT time, not local time

--- source/resources/js/BookEntry.test.js
+++ source/resources/js/BookEntry.test.js
@@ -11,7 +11,7 @@ test('valid constructor', () => {
   //const testBookEntryForConstructor = new BookEntry(new Set(['Sci-fi']), new Date('November 17, 2022 03:24:00'), 9, 340, 'completed', 'I thought it was poignantly pointless.', 'A4238A53I1', 'Robert Heinlein', 'Stranger in a Strange Land')  ;  
   const testBookEntryForConstructor = BookEntryClass.bookentry(new Set(['Sci-fi', 'Fiction']), new Date('November 17, 2022 03:24:00'), 9, 340, 'completed', 'I thought it was poignantly pointless.', 'A4238A53I1', 'Robert Heinlein', 'Stranger in a Strange Land')  ;  
 
-  expect(testBookEntryForConstructor.printBookEntry()).toBe('Stranger in a Strange Land by: Robert Heinlein, rating: 9, progress: 340, status: completed, Date read: Thu Nov 17 2022 03:24:00 GMT-0800 (Pacific Standard Time) \nReview: I thought it was poignantly pointless., ISBN: A4238A53I1, tags: Sci-fi, Fiction');
+  expect(testBookEntryForConstructor.printBookEntry()).toBe('Stranger in a Strange Land by: Robert Heinlein, rating: 9, progress: 340, status: completed, Date read: Thu Nov 17 2022 03:24:00 GMT+0000 (Pacific Standard Time) \nReview: I thought it was poignantly pointless., ISBN: A4238A53I1, tags: Sci-fi, Fiction');
 }); 
 
 test('empty string constructor', () => {
