a7dcf1f90c08eb53ac9050179ed51f078efe6093
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-29 12:43:18 -0800

    Create bookEntries.js

---
+++ source/bookEntries/bookEntries.js
@@ -0,0 +1,7 @@
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";  
