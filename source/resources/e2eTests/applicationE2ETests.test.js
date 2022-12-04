0c251646b88fdd811716b263d37e3e176a621543
adaburton <theadaburton@gmail.com>
2022-12-03 16:40:40 -0800

    Will add a list and make sure it is added

--- source/resources/e2eTests/applicationE2ETests.test.js
+++ source/resources/e2eTests/applicationE2ETests.test.js
@@ -2,23 +2,19 @@ const { Dialog, default: puppeteer } = require("puppeteer");
 
 describe('Basic user flow for Website', () => {
     beforeAll(async () => {
-      const browser = await puppeteer.launch({headless: false, slowMo: 10});
-      const page = await browser.newPage();
+      
       
       });
 
 //checking book entries page, main functionality page
       it('check icon on bookentries page', async () => {
         console.log('Checking icon img on bookentries page...');
-        const browser = await puppeteer.launch({headless: false, slowMo: 10});
-        const page = await browser.newPage();
+       
         await page.goto('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/bookEntries/bookEntries.html');
         // Query select the icon img
         const images = await page.$$('img');
         const src = await images[0].getProperty('src');
         const text = await src.jsonValue();
-
-        await browser.close();
 
         expect(text).toBe('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/resources/images/vectors/Logo.svg');
       }, 2500);
@@ -27,8 +23,7 @@ describe('Basic user flow for Website', () => {
 //checking homepage      
     //check start button
     it('check homepage start button text', async () => {
-        const browser = await puppeteer.launch({headless: false, slowMo: 10});
-        const page = await browser.newPage();
+        
         await page.goto('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/homepage/index.html');
         console.log('Checking start button...');
         // Query select the start button
@@ -36,15 +31,14 @@ describe('Basic user flow for Website', () => {
         const data = await button.getProperty('textContent');
         const text = await data.jsonValue();
 
-        await browser.close();
+        
 
         expect(text).toBe('Start now');
       }, 2500);
 
     //check learn more button
     it('check homepage learn more button text', async () => {
-        const browser = await puppeteer.launch({headless: false, slowMo: 10});
-        const page = await browser.newPage();
+        
         await page.goto('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/homepage/index.html');
         console.log('Checking learn more button...');
         // Query select the learn more button
@@ -52,15 +46,12 @@ describe('Basic user flow for Website', () => {
         const data = await buttons[1].getProperty('textContent');
         const text = await data.jsonValue();
         
-        await browser.close();
-
+        
         expect(text).toBe('Learn more');
       }, 2500);
   
     //check icon image
     it('check homepage icon image', async () => {
-        const browser = await puppeteer.launch({headless: false, slowMo: 10});
-        const page = await browser.newPage();
         await page.goto('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/homepage/index.html');
         console.log('Checking icon img...');
         // Query select the icon img
@@ -68,7 +59,7 @@ describe('Basic user flow for Website', () => {
         const src = await images[0].getProperty('src');
         const text = await src.jsonValue();
 
-        await browser.close();
+       
 
         expect(text).toBe('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/resources/images/vectors/Logo.svg');
       }, 2500);
@@ -76,32 +67,32 @@ describe('Basic user flow for Website', () => {
 
       //check the add List buttton
       it('Check that the add-List button works', async () =>{
-        const browser = await puppeteer.launch({headless: false, slowMo:100});
-        const page = await browser.newPage();
         await page.goto('https://cse110-fa22-group13.github.io/cse110-fa22-group13/source/bookEntries/bookEntries.html');
         console.log("Testing the 'add list' button...");
         // Query select the add List button
         const listButton = await page.$('#add-entry .add-button');
 
         let buttonText = await listButton.getProperty('innerText');
+
         buttonText = await buttonText.jsonValue();
 
-        // click the list and type a name for the new list
-        listButton.click();
-
-        // 
         page.on('dialog', async dialog => {
-          console.log(dialog.type());
-          console.log(dialog.message());
+          console.log(await dialog.message());
           await dialog.accept("Test");
         });
 
+        // click the list and type a name for the new list
+        await listButton.click();
+
         const list = await page.$('book-list');
-        console.log(list);
+        const shadowRoot = await list.getProperty('shadowRoot');
+        let label = await shadowRoot.$('.list-title');
+        label = await label.getProperty('innerText');
+        label = await label.jsonValue();
         
         
         
-        expect().toBe('Test');
+        expect(label).toBe('Test');
       }, 10000);
 
   });
