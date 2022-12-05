9ef3648143d638bba2cb03272484b270765a21af
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-12-04 23:17:59 -0800

    Revert "Updated HTML and CSS "

--- source/bookEntries/bookEntries.js
+++ source/bookEntries/bookEntries.js
@@ -1,81 +1,84 @@
 window.addEventListener('DOMContentLoaded', init);
 /**
  * This function initializes the webpage by first fetching from local storage, displaying all current book entries in their list, in they exist.
- *
+ * 
  */
 async function init() {
-  try {
-    await fetchItems();
-  } catch (err) {
-    console.log(`Error fetch items: ${err}`);
-    return; // Return if fetch fails
-  }
-  const addButton = document.querySelector('.add-list-btn');
-  addButton.addEventListener('click', addNewList);
+    try {
+        await fetchItems();
+    } 
+    catch (err) {
+        console.log(`Error fetch items: ${err}`);
+        return; // Return if fetch fails
+    }
+    let addButton = document.querySelector('.add-button');
+    addButton.addEventListener('click', addNewList);
 }
 
 /**
  * This function adds a new list to the page using the name given by the user
  */
-function addNewList() {
-  const label = prompt('Name of new list?');
-  if (label != null) {
-    const testList = document.createElement('book-list');
-    const emptyList = [];
-    if (localStorage.getItem('listOfNames') === null) {
-      localStorage.setItem(label, JSON.stringify(emptyList));
-      emptyList.push(label);
-      localStorage.setItem('listOfNames', JSON.stringify(emptyList));
-    } else {
-      const newEntries = JSON.parse(localStorage.getItem('listOfNames'));
-      for (let i = 0; i < newEntries.length; i++) {
-        if (newEntries[i] == label) {
-          return;
+function addNewList(){
+    let label = prompt("Name of new list?");
+    if(label != null){
+        let testList = document.createElement('book-list');
+        let emptyList = [];
+        if(localStorage.getItem("listOfNames") === null){
+            localStorage.setItem(label, JSON.stringify(emptyList));
+            emptyList.push(label);
+            localStorage.setItem("listOfNames", JSON.stringify(emptyList));
         }
-      }
-      newEntries.push(label);
-      localStorage.setItem(label, JSON.stringify(emptyList));
-      localStorage.setItem('listOfNames', JSON.stringify(newEntries));
+        else{
+            let newEntries = JSON.parse(localStorage.getItem("listOfNames"));
+            for (var i = 0; i < newEntries.length; i++) {
+                if(newEntries[i] == label){
+                    return;
+                }
+            }
+            newEntries.push(label);
+            localStorage.setItem(label, JSON.stringify(emptyList));
+            localStorage.setItem("listOfNames", JSON.stringify(newEntries));
+        }
+        testList.setListLabel(label);
+        document.querySelector('#bookList').appendChild(testList);
     }
-    testList.setListLabel(label);
-    document.querySelector('#bookList').appendChild(testList);
-  }
 }
 
 /**
  * This function finds out what needs to be populated based on the contents of localStorage, and then fetches them from local storage
  */
 async function fetchItems() {
-  return new Promise( (resolve, reject) => {
-    if (localStorage.getItem('listOfNames') === null) {
-      resolve();
-    } else {
-      const storedInfo = localStorage.getItem('listOfNames');
-      const newLists = JSON.parse(storedInfo);
-      for (let i = 0; i < newLists.length; i++) {
-        const testList = document.createElement('book-list');
-        const section = testList.shadowRoot.querySelector('section');
-        section.id = 'new-list' + i;
-        testList.setListLabel(newLists[i]);
-        document.querySelector('#bookList').appendChild(testList);
-
-        const newEntries = JSON.parse(localStorage.getItem(section.id));
-        const entries = testList.shadowRoot.querySelector('.entries');
-
-        if (newEntries !== null) {
-          for (let j = 0; j < newEntries.length; j++) {
-            const testEntry = document.createElement('book-entry');
-            const storeListID = testEntry.shadowRoot.querySelector('.entry');
-            storeListID.classList.add('new-list' + i);
-            const retrievedData = newEntries[j];
-            const newEntry = JSON.parse(localStorage.getItem(retrievedData));
-            testEntry.data(newEntry[0]);
-            storeListID.classList.add(newEntries[j]);
-            entries.appendChild(testEntry);
-          }
+    return new Promise(async (resolve, reject) => {
+        if(localStorage.getItem("listOfNames") === null){
+            resolve();
         }
-      }
-      resolve();
-    }
-  });
+        else{
+            const storedInfo = await localStorage.getItem("listOfNames");
+            const newLists = await JSON.parse(storedInfo);
+            for (let i = 0; i < newLists.length; i++) {
+                const testList = await document.createElement('book-list');
+                const section = await testList.shadowRoot.querySelector('section');
+                section.id = 'new-list' + i;
+                testList.setListLabel(newLists[i]);
+                document.querySelector('#bookList').appendChild(testList);
+                
+                const newEntries = await JSON.parse(localStorage.getItem(section.id));
+                const entries = await testList.shadowRoot.querySelector('.entries');
+                
+                if(newEntries !== null){
+                    for (let j = 0; j < newEntries.length; j++) {
+                        const testEntry = document.createElement('book-entry');
+                        const storeListID = testEntry.shadowRoot.querySelector('.entry');
+                        storeListID.classList.add('new-list' + i);
+                        const retrievedData = newEntries[j];
+                        const newEntry = await JSON.parse(localStorage.getItem(retrievedData));
+                        testEntry.data(newEntry[0]);  
+                        storeListID.classList.add(newEntries[j]);
+                        entries.appendChild(testEntry);
+                    }
+                }
+            }
+            resolve();
+        }
+    });
 }
--- source/bookEntries/bookEntries.test.js
+++ source/bookEntries/bookEntries.test.js
@@ -2,13 +2,14 @@
  * @jest-environment jsdom
  */
 
-// test constructor
+//test constructor
 test('not null constructor', () => {
-  const bookEntries = document.createElement('book-entries');
-  expect(bookEntries).not.toBeNull();
-});
-test('set to null', () => {
-  let bookEntries = document.createElement('book-entries');
-  bookEntries = null;
-  expect(bookEntries).toBeNull();
-});
+    const bookEntries = document.createElement('book-entries');
+    expect(bookEntries).not.toBeNull();
+  }); 
+  test('set to null', () => {
+    let bookEntries = document.createElement('book-entries');
+    bookEntries = null;
+    expect(bookEntries).toBeNull();
+  }); 
+  
